449f14cf4bfc17afc681f33b7449aec0a010c807
GersonBenito <gersonbenito4@gmail.com>
2023-01-01 14:53:34 -0600

    feat: formulario maquetado y manejo del formulario agregado

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
-const btnFloat = document.querySelector('.button-float');
-const containerFooter = document.querySelector('.container');
+document.addEventListener('DOMContentLoaded', function(){
 
+    const btnFloat = document.querySelector('.button-float');
+    const containerFooter = document.querySelector('.container');
 
-const toggleAction = () =>{
-
-    // verificar si se esta mostrando el contenido del footer
-    if(containerFooter.classList.contains('active')){
-        // si se esta mostrando, lo removeremos al darle click al boton
-        containerFooter.classList.remove('active');
-        btnFloat.classList.remove('btn-active');
-        btnFloat.textContent = 'More Information';
-    }else{
-        containerFooter.classList.add('active');
-        btnFloat.classList.add('btn-active');
-        btnFloat.textContent = 'X Close';
+    // form
+    const inputName = document.querySelector('#name');
+    const inputLastName = document.querySelector('#lastName');
+    const inputAge = document.querySelector('#age');
+    const inputEmail = document.querySelector('#email');
+    const inputUserName = document.querySelector('#userName');
+    
+    const toggleAction = () =>{
+    
+        // verificar si se esta mostrando el contenido del footer
+        if(containerFooter.classList.contains('active')){
+            // si se esta mostrando, lo removeremos al darle click al boton
+            containerFooter.classList.remove('active');
+            btnFloat.classList.remove('btn-active');
+            btnFloat.textContent = 'More Information';
+        }else{
+            containerFooter.classList.add('active');
+            btnFloat.classList.add('btn-active');
+            btnFloat.textContent = 'X Close';
+        }
     }
-}
-btnFloat.addEventListener('click', toggleAction);
+
+    btnFloat.addEventListener('click', toggleAction);
+
+    // eventos del formulario
+    inputName.addEventListener('blur', validator);
+    inputLastName.addEventListener('blur', validator);
+    inputAge.addEventListener('blur', validator);
+    inputEmail.addEventListener('blur', validator);
+    inputUserName.addEventListener('blur', validator);
+
+    // funcion validar
+    function validator({ target }){
+        console.log(target.value);
+    }
+
+});
